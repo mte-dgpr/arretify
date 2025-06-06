3d747276fb65f3fbca0aea1ad85fa691506b42d2
Sébastien Piquemal <sebastien.piquemal@developpement-durable.gouv.fr>
2025-06-06 14:36:23 +0200

    added licence text and hooks

--- arretify/debug.py
+++ arretify/debug.py
@@ -16,3 +16,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from typing import Iterable, List
+
+from bs4 import BeautifulSoup
+
+from .utils.functional import flat_map_string
+from .regex_utils import (
+    split_string_with_regex,
+    map_matches,
+    PatternProxy,
+)
+from .utils.html import make_data_tag
+from .html_schemas import DEBUG_KEYWORD_SCHEMA
+from .types import PageElementOrString
+
+
+def insert_debug_keywords(
+    soup: BeautifulSoup,
+    children: Iterable[PageElementOrString],
+    query: str,
+) -> List[PageElementOrString]:
+    pattern = PatternProxy(query)
+    return list(
+        flat_map_string(
+            children,
+            lambda string: map_matches(
+                split_string_with_regex(pattern, string),
+                lambda match: make_data_tag(
+                    soup,
+                    DEBUG_KEYWORD_SCHEMA,
+                    contents=[str(match.group(0))],
+                    data=dict(query=query),
+                ),
+            ),
+        )
+    )
