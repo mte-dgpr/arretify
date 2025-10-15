314a3f506ef910edc24ed9ac53f7b412e189c294
sebastienmeyer2 <sebastien.meyer36@gmail.com>
2025-10-15 17:41:14 +0200

    rework downloading data from georisques
    add logging of downloaded files in table

---
+++ scripts/download_data_georisques.py
@@ -0,0 +1,232 @@
+#
+# Copyright (c) 2025 Direction générale de la prévention des risques (DGPR).
+#
+# This file is part of Arrêtify.
+# See https://github.com/mte-dgpr/arretify for further info.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+import logging
+import time
+import csv
+from datetime import datetime
+from optparse import OptionParser
+from pathlib import Path
+from typing import Dict, Optional
+
+import requests
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+CATCHED_FILE_TYPES = [
+    # Should be the arretes we are looking for, however some might be wrongly classified
+    "ap d'autorisation",
+    "ap enregistrement",
+    "arrêté préfectoral",
+    "ap servitude d'utilité publique",
+    "ap prescriptions complémentaires",
+    # Should not be the arretes we are looking for, however some might be wrongly classified
+    "rapport",
+    "document de procédure",
+    "fiche seveso",
+    "inspection",
+    "rapport d'ap d'autorisation",
+    "arrêté de mise en demeure",
+    "ap mise en demeure",
+    "ap levée de mise en demeure",
+    "ap mesures conservatoires",
+    "ap autorisation temporaire",
+    "ap mesures d'urgence",
+    # For these we do not know beforehand
+    "autre",
+]
+
+FILES_COUNTER: Dict[str, int] = {file_type: 0 for file_type in CATCHED_FILE_TYPES}
+
+FILES_NAMES: Dict[str, list[str]] = {file_type: [] for file_type in CATCHED_FILE_TYPES}
+
+FILES_LIST: list[tuple[str, str, str, str]] = []
+
+
+def get_icpe_data(code_aiot: str) -> Dict:
+
+    url = f"https://georisques.gouv.fr/api/v1/installations_classees?codeAIOT={code_aiot}"
+
+    response = requests.get(url, headers={"accept": "application/json"})
+
+    if response.status_code != 200:
+        raise requests.HTTPError(response=response)
+
+    icpe_data = response.json()
+    if icpe_data.get("results", 0) == 0:
+        return {}
+
+    return icpe_data["data"][0]
+
+
+def process_icpe_data(
+    icpe_data: Dict, code_aiot: str, out_dir: Optional[Path], sleep_time: float = 0.5
+):
+
+    icpe_documents = icpe_data.get("documentsHorsInspection", [])
+    if not icpe_documents:
+        _LOGGER.warning(f"No document found for ICPE {code_aiot}")
+
+    if out_dir:
+        icpe_dir = out_dir / code_aiot
+        if not icpe_dir.is_dir():
+            os.mkdir(icpe_dir)
+
+    for document_data in icpe_documents:
+
+        file_date = document_data["dateFichier"]
+
+        file_type = document_data["typeFichier"].lower()
+        if file_type not in CATCHED_FILE_TYPES:
+            err_msg = (
+                f"Unknown file type '{file_type}', please specify if it should be catched or not"
+            )
+            raise KeyError(err_msg)
+
+        file_name = document_data["nomFichier"]
+        file_name = file_name.replace("/", "-")  # avoid issues with paths
+        file_name = file_name.replace("*", "")  # avoid issues with paths
+        FILES_COUNTER[file_type] += 1
+        FILES_NAMES[file_type].append(file_name)
+        FILES_LIST.append((f"'{code_aiot}", file_date, file_type, file_name))
+
+        file_name_save = f"{file_date}_{file_type}_{file_name}.pdf"
+
+        if out_dir:
+
+            file_path = icpe_dir / file_name_save
+            if file_path.is_file():
+                _LOGGER.info(f"File {file_name_save} already exists, continue...")
+                continue
+
+            response = requests.get(document_data["urlFichier"], stream=True)
+
+            if response.status_code == 500:
+                _LOGGER.warning(f"File {file_name_save} not existing on server side, continue...")
+                time.sleep(sleep_time)  # avoid error 503
+                continue
+
+            with open(file_path, "wb") as f:
+                for chunk in response.iter_content(chunk_size=8192):
+                    f.write(chunk)
+            _LOGGER.info(f"Downloaded file {file_name_save}")
+            time.sleep(sleep_time)  # avoid error 503
+
+        else:
+
+            _LOGGER.info(f"Seen file {file_name_save}")
+
+
+def download_one_icpe(code_aiot: str, out_dir: Optional[Path]):
+
+    try:
+        icpe_data = get_icpe_data(code_aiot)
+    except requests.HTTPError as err:
+        _LOGGER.warning(f"Failed to get ICPE {code_aiot} from georisques")
+        _LOGGER.warning(err)
+
+    process_icpe_data(icpe_data, code_aiot, out_dir)
+
+
+def iter_icpe_file(file_path: Path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        for line in file:
+            yield line.strip()
+
+
+def initialize_logger(log_dir: Optional[Path]):
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter("%(levelname)s - %(message)s"))
+    handlers: list[logging.StreamHandler] = [stream_handler]
+
+    if log_dir and not log_dir.is_dir():
+        os.mkdir(log_dir)
+
+    if log_dir:
+        log_dir.mkdir(exist_ok=True)
+        log_file = log_dir / f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+        handlers.append(logging.FileHandler(log_file, encoding="utf-8"))
+
+    logging.basicConfig(level=logging.INFO, handlers=handlers)
+
+
+def main(icpe_file: Optional[Path], out_dir: Optional[Path], log_dir: Optional[Path]):
+    """Run downloading of ICPE documents from georisques.gouv.fr
+
+    Args:
+        icpe_file (Optional[Path]): Path to a file containing one ICPE code AIOT per line
+        out_dir (Optional[Path]): Dir where to save downloaded files, if None files are not saved
+        log_dir (Optional[Path]): Dir where to save log file, if None no log file is saved
+    """
+    if not icpe_file:
+        err_msg = "No file provided for ICPE list, please provide one with -i or --icpe-file"
+        raise KeyError(err_msg)
+
+    if out_dir and not out_dir.is_dir():
+        os.mkdir(out_dir)
+
+    initialize_logger(log_dir)
+
+    for code_aiot in iter_icpe_file(icpe_file):
+
+        _LOGGER.info(f"--- ICPE {code_aiot}")
+        download_one_icpe(code_aiot, out_dir)
+
+    _LOGGER.info("--- Summary ---")
+    for file_type, count in FILES_COUNTER.items():
+        _LOGGER.info(f"Seen or downloaded {count} files of type '{file_type}'")
+        _LOGGER.info(f"Files: {FILES_NAMES[file_type]}")
+
+    if log_dir:
+        file_list_name = log_dir / f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.csv"
+        with open(file_list_name, mode="w", newline="", encoding="utf-8-sig") as f:
+            writer = csv.writer(f, delimiter=";")
+            writer.writerow(["Code AIOT", "dateFichier", "typeFichier", "nomFichier"])
+            writer.writerows(FILES_LIST)
+
+
+if __name__ == "__main__":
+
+    parser = OptionParser()
+
+    parser.add_option(
+        "-i",
+        "--icpe-file",
+        default=None,
+    )
+    parser.add_option(
+        "-o",
+        "--out-dir",
+        default=None,
+    )
+    parser.add_option(
+        "-l",
+        "--log-dir",
+        default=None,
+    )
+    (options, args) = parser.parse_args()
+
+    icpe_file = Path(options.icpe_file) if options.icpe_file else None
+    out_dir = Path(options.out_dir) if options.out_dir else None
+    log_dir = Path(options.log_dir) if options.log_dir else None
+
+    main(icpe_file, out_dir, log_dir)
